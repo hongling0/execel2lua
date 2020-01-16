1321d2711e66e80545364c1d6f1659f9912c576b
王红 <hongling-0@qq.com>
2020-01-16 11:26:28 +0800

    移动代码到src目录

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 echo off
-rd /s /Q dist\
-pyinstaller execl2lua.py -F
-copy /y cfg.ini dist\
-copy /y alise.txt dist\
+cd src
+rd /s /Q ..\dist
+pyinstaller execl2lua.py -F --distpath=..\dist
+copy /y cfg.ini ..\dist
+copy /y alise.txt ..\dist
+cd ..
 pause
